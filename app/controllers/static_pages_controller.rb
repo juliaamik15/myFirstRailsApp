49583ad7330625bf25b3fc7858f854b4b7f5fd86
juliaamik15 <juliaamik15@gmail.com>
2016-07-18 14:11:27 +0100

    gallery page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,4 @@
 class StaticPagesController < ApplicationController
-  def faq
+  def gallery
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_pages/gallery'
+
   resources :products
   get 'static_pages/faq'
 
