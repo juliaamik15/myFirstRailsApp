4ddb254def32f49a7be68d211c0ae7f2d69e1a80
juliaamik15 <juliaamik15@gmail.com>
2016-07-19 14:09:08 +0100

    4.3 order controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,8 @@
 class StaticPagesController < ApplicationController
   def gallery
   end
+
+  def landing_page
+  	@featured_product = Product.first
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,8 @@ Rails.application.routes.draw do
   #root 'welcome#index'
   root 'static_pages#index'
 
+  resources :orders, only: [:index, :show, :create, :destroy]
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
