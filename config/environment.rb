16113fe25a3c05a297ee44fce87a6c3e120d9fa0
juliaamik15 <juliaamik15@gmail.com>
2016-09-12 15:12:48 +0100

    5.3 mail sending

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
   :address => 'smtp.sendgrid.net',
   :port => '587',
   :authentication => :plain,
-  :user_name => 'juliaamik15@gmail.com',
-  :password => 'Rubens15',
+  :user_name => 'app53284932@heroku.com',
+  :password => 'uuehjupg3621',
   :domain => 'heroku.com',
   :enable_starttls_auto => true
 }
