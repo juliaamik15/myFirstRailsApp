1beaa6137f299c3775094d7e8042d9e2564f430a
juliaamik15 <juliaamik15@gmail.com>
2016-09-20 13:01:31 +0100

    comments

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,16 @@
 class Product < ActiveRecord::Base
 	has_many :orders
+	has_many :comments
+
+	def highest_rating_comment
+  	comments.rating_desc.first
+	end
+
+	def worst_rating_comment
+  	comments.rating_asc.first
+	end
+
+	def comments_count
+		comments.count
+	end	
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 	has_many :orders
-
+	has_many :comments
 	
 end
