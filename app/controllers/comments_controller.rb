22d0380019db2b7ee79b23c3a2b069411bdf3eb1
juliaamik15 <juliaamik15@gmail.com>
2016-09-28 11:42:18 +0100

    5.10 admin role

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,6 +17,10 @@ class CommentsController < ApplicationController
   end
 
   def destroy
+    @comment = Comment.find(params[:id])
+    product = @comment.product
+    @comment.destroy
+    redirect_to product
   end
 
   private
@@ -24,5 +28,7 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:user_id, :body, :rating)
   end
+
+
   
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,8 +4,14 @@ class Ability
   def initialize(user)
 
     user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
-    can [:read, :create, :update], Order, :user_id => user.id
+    if user.admin?
+      can :manage, :all
+    else
+      can :read, :all
+      can :manage, User, id: user.id
+      can [:read, :create], Comment
+      can [:read, :create, :update], Order, :user_id => user.id
+    end
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
