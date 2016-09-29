c1040373c92947f1f6f30aa7f7bcc8f52f9b04cd
juliaamik15 <juliaamik15@gmail.com>
2016-09-29 09:34:58 +0100

    devise saves the first and last name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::Base
+	before_filter :update_sanitized_params, if: :devise_controller?
+
+	def update_sanitized_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :first_name, :last_name])
+	end
+
 	rescue_from CanCan::AccessDenied do |exception|
   	redirect_to main_app.root_url, :alert => exception.message
 	end
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  
 end
