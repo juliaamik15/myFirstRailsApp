3ec7d84d25749343be20d87590b97b342dae0bec
juliaamik15 <juliaamik15@gmail.com>
2016-11-02 10:10:07 +0000

    no cache for search fix

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -32,7 +32,7 @@ class ProductsController < ApplicationController
   # POST /products.json
   def create
     @product = Product.new(product_params)
-    byebug
+    #byebug
     respond_to do |format|
       if @product.save
         format.html { redirect_to products_path, notice: 'Product was successfully created.' }
@@ -77,5 +77,9 @@ class ProductsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
       params.require(:product).permit(:name, :description, :image_url, :flower_colour, :price, :availability)
+    end
+
+    def expire_cache
+      ActionController::Base.new.expire_fragment(cache_key_for_products)
     end
 end
